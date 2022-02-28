96ce3d0b6e44b611b0bb89e016ab03d420b32c10
Jake Keller <jakekeller321@gmail.com>
2022-02-28 15:44:46 -0500

    Started on a weather application

---
+++ app/src/main/java/com/example/weatherapplication/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapplication.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavoriteScreen,
+    SearchScreen,
+    SettingsScreen
+}

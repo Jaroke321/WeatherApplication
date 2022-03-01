0fdb055313e5011258ce0845ad57b66b8fa27e64
Jake Keller <jakekeller321@gmail.com>
2022-03-01 18:31:54 -0500

    Updated the splash screen with an animation and created a main screen for after the splash screen.

--- app/src/main/java/com/example/weatherapplication/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/example/weatherapplication/navigation/WeatherNavigation.kt
@@ -4,14 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.weatherapplication.screens.WeatherSplashScreen
+import com.example.weatherapplication.screens.main.MainScreen
+import com.example.weatherapplication.screens.splash.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
+
         composable(WeatherScreens.SplashScreen.name) {
             WeatherSplashScreen(navController = navController)
+        }
+
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController = navController)
         }
     }
 }
